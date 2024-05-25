47d3681be12fe21e03ece36c2f6e9f06a602a861
thomasbazeille <bazeille.thomas@gmail.com>
2024-05-26 00:43:55 +0200

    tone

--- apps/backend/services/createStories.py
+++ apps/backend/services/createStories.py
@@ -14,21 +14,40 @@ genre_characteristics = {"fairy tale":"Incorporate elements of magic, moral less
                    "mystery story":"Describe the clues and teamwork they use, with a child-friendly approach and a light and engaging tone. ",
                    "futuristic story":"Incorporate futuristic elements, imaginative settings, and technological themes, while maintaining a playful and adventurous tone. "}
 
+
+tone_list= ["poetic","serious","funny","whimsical","melancholic","dark","optimistic","reflective"]
+
+tone_characteristics= {"poetic":"Employs figurative language, rhythm, and vivid imagery to evoke emotions and create a lyrical quality : expressive and evocative storytelling. ",
+                        "serious": "Convey gravity and importance through a focus on significant themes and an earnest approach, providing depth and weight. ",
+                        "funny": "Use humor, jokes and a light-hearted style to entertain the reader. ",
+                        "whimsical": "Feature playful and imaginative elements, or a sense of wonder and fantasy. ",
+                        "melancholic": "Express sadness or a contemplative and introspective approach, targeting emotional depth and complexity to characters and plots. ",
+                        "dark": "Explore themes of gloom, mystery or foreboding with a somber tone to create tension and depict the more sinister aspect of human experience. ",
+                        "optimistic": "Present a hopeful and inspiring perspective. ",
+                        "reflective": "Offer deep thoughts and contemplation, possibily exploring personal insights and experience to add introspective depth and philosophoical weight to the plot or characters. "}
+
 def parseStories(text):
     story_pattern = re.compile(r"<story>(.*?)</story>", re.DOTALL)
     stories = story_pattern.findall(text)
     stories = [story.strip() for story in stories]
     return stories
 
-def createStories(topic, n_stories=3, temperature=0.7, size=StorySize.VERY_SHORT,genre=None):
+def createStories(topic, n_stories=3, temperature=0.7, size=StorySize.VERY_SHORT,genre=None, tone=None):
     client = MistralClient(api_key=MISTRAL_API_KEY)
     number_words = int(size.value * 3 / 5)
 
     if genre and genre in genre_list:
         additional = genre_characteristics[genre]
+        genre = " " + genre
     else:
         genre=""
         additional=""
+
+    if tone and tone in tone_list:
+        tone = " " + tone
+        additional += tone_characteristics[tone]
+    else:
+        tone = ""
 
     chat_response = client.chat(
         model=MISTRAL_MODEL,
@@ -37,7 +56,7 @@ def createStories(topic, n_stories=3, temperature=0.7, size=StorySize.VERY_SHORT
             ChatMessage(
                 role="user",
                 content="""
-                    Propose {n_stories} {genre} stories between tags '<story>' '</story>' about {topic} in {number_words} words.
+                    Propose {n_stories}{tone}{genre} stories between tags '<story>' '</story>' about {topic} in {number_words} words.
                     Ensure each story has:
                     1. A different setting.
                     2. Unique characters with specific traits.
